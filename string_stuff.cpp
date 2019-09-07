1301edcd470e5a9be115f0f0e26d114e7408163b
MethleshKoshle <45479114+MethleshKoshle@users.noreply.github.com>
2019-09-07 16:25:04 +0530

    Update string_stuff.cpp

--- string_stuff.cpp
+++ string_stuff.cpp
@@ -54,3 +54,23 @@ ll stoI(string n){
 stoi(num);//integer range
 stoll(num);//ll range
 stoull(num);//ull range
+
+vector<string> get_words_from_line()
+{
+  string line, s, tmp="";
+  vector<string> var;
+  getline(cin, line);
+  getline(cin, line);
+  line+=" ";
+  for(auto e:line)
+  {
+    if(e==' ' and tmp.length())
+    {
+      var.pb(tmp);
+      tmp="";
+    }
+    else
+      tmp+=e;
+  }
+  return var;
+}
