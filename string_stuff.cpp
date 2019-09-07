05117e0f425149c90d085206de69cf92b79f4b28
MethleshKoshle <45479114+MethleshKoshle@users.noreply.github.com>
2019-09-07 16:23:51 +0530

    Create string_stuff.cpp

---
+++ string_stuff.cpp
@@ -0,0 +1,56 @@
+#define all(x) x.begin(), x.end()
+
+// take string input using scanf()
+char tmp[101];
+scanf("%100s", tmp);
+string a= tmp;
+
+//////////// Convert deciaml integer to binary ////////////
+inline string bin(int x)
+{
+    string n="";
+    char c;
+    while(x)
+    {
+        c='0'+x%2;
+        n = n+string{c};
+        x/=2;
+    }
+    reverse(all(n));
+    return n;
+}
+
+// convert intger leteral to string
+template <class T>
+inline std::string to_string (const T& t)
+{
+    std::stringstream ss;
+    ss << t;
+    return ss.str();
+}
+
+/*convert integer literal to string*/
+string itos(ll x){
+    string tmp="";
+    do{
+        tmp+=to_string(x%10);
+        x/=10;
+    }while(x>0);
+    reverse(all(tmp));
+    return tmp;
+}
+
+//convert string literal to long long int
+ll stoI(string n){
+    ll tmp=0, fact=1;
+    wb(i, n.length()-1, 0, 1){
+        tmp+=stoi(string{n[i]})*fact;
+        fact*=10;
+    }
+    return tmp;
+}
+
+//aliter
+stoi(num);//integer range
+stoll(num);//ll range
+stoull(num);//ull range
